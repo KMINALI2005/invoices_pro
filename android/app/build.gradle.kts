fecb8ee893292cc58e0f8a16d3e5a579f3b9d5f8
KMINALI2005 <162717637+KMINALI2005@users.noreply.github.com>
2025-10-25 01:43:44 +0300

    Fix versionCode and versionName method calls

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,8 +26,11 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        
+        // --- (هنا تم الإصلاح) ---
+        versionCode = flutter.versionCode()
+        versionName = flutter.versionName()
+        // --- (انتهى الإصلاح) ---
     }
 
     buildTypes {
