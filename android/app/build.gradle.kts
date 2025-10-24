0eeb7c6b9545f41ae03b6cb3219632280f4c3b37
KMINALI2005 <162717637+KMINALI2005@users.noreply.github.com>
2025-10-25 01:58:25 +0300

    Fix ndkVersion in build.gradle.kts
    Updated ndkVersion to a specific version for compatibility.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,10 @@ plugins {
 android {
     namespace = "com.example.invoices_pro"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    
+    // --- (هنا تم الإصلاح) ---
+    ndkVersion = "27.0.12077973"
+    // --- (انتهى الإصلاح) ---
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -20,23 +23,15 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.invoices_pro"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        
-        // --- (هنا تم الإصلاح) ---
         versionCode = flutter.versionCode()
         versionName = flutter.versionName()
-        // --- (انتهى الإصلاح) ---
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
